57d2a63dc3e4a7cfc29c919eec6416bd867ebcb4
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-06 13:10:03 -0500

    monadexample1.hs: Some unnecessary crap is removed.

--- monads/monadexample1.hs
+++ monads/monadexample1.hs
@@ -1,9 +1,6 @@
 import Control.Monad;
 import System.Environment;
 
-runNonIO :: a -> IO a;
-runNonIO = return;
-
 g :: [String] -> String;
 g x | length x == 0 = "I need a name, dumb-ass."
     | otherwise = "Fuck you, " ++ x !! 0 ++ "."
