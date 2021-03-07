1e364ec1ace0ac6290d216950351ff2eb24f8791
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-07 15:14:59 -0500

    monadexample3.hs: The lengths of some lines are decreased.

--- monads/monadexample3.hs
+++ monads/monadexample3.hs
@@ -1,9 +1,15 @@
 useless :: Monad m => a -> (a -> m b) -> m b;
 useless a b = (>>=) (return a) b;
---\texttt{useless} encases the first input into a monad, ``extracts'' the value of the resulting monad, sends this value to function \texttt{b}, and outputs a monad.
+-- \texttt{useless} encases the first input into a monad, ``extracts''
+-- the value of the resulting monad, sends this value to function
+-- \texttt{b}, and outputs a monad.
 
---return converts \texttt{a}'s type from \texttt{a} to \texttt{[ARBITRARY MONAD] a}.
---\texttt{(>>=)} ``extracts'' the \texttt{a} value of \texttt{[ARBITRARY MONAD] a} and applies function \texttt{b} to this ``extracted'' value, thereby outputting a value whose type is shared with the output type of function \texttt{b}.
+-- return converts \texttt{a}'s type from \texttt{a} to
+-- \texttt{[ARBITRARY MONAD] a}.
+-- \texttt{(>>=)} ``extracts'' the \texttt{a} value of
+-- \texttt{[ARBITRARY MONAD] a} and applies function \texttt{b} to this
+-- ``extracted'' value, thereby outputting a value whose type is shared
+-- with the output type of function \texttt{b}.
 
 main :: IO ();
 main = useless "Fuck you."  putStrLn;
