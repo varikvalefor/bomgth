458860409813eb1ab5747f5b699281763d995563
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-14 21:33:15 -0400

    shat.hs: Command d is added.

--- relativelypracticalstuff/lecturex/shat.hs
+++ relativelypracticalstuff/lecturex/shat.hs
@@ -24,12 +24,17 @@ edWrite :: [String] -> String -> IO [String];
 edWrite buf fn = writeFile fn conkd >> return buf
   where conkd = (foldr (++) [] . intersperse "\n") buf
 
+edDel :: String -> [String] -> IO [String];
+edDel cmd buf = return $ take (n - 1) buf ++ drop n buf
+  where n = read $ init cmd :: Int
+
 edFunction :: [String] -> IO ();
 edFunction buf = getLine >>= detFun >>= edFunction
   where detFun cmd | length cmd == 0 = err
                    | last cmd == 'p' = edPrintLine cmd buf
                    | last cmd == 'i' = edInsertLine cmd buf
                    | head cmd == 'w' = edWrite buf (drop 2 cmd)
+                   | last cmd == 'd' = edDel cmd buf
                    | otherwise = err
         err = putStrLn "?" >> return buf
 
