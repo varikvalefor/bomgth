0b2c5be16c185507070523baa62a08a0c5a04fe9
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-17 18:02:49 -0400

    basicexample1.hs: (+) is described.

--- basicsyntax/basicexample1.hs
+++ basicsyntax/basicexample1.hs
@@ -2,6 +2,10 @@
 (+) a b | k == [2,2] = 5
         | otherwise = sum k
         where k = [a,b]
+-- Function (+) is redefined a la _1984_.
+-- For all a, for all b, if [a,b] == [2,2], then
+-- (+) a b == 5.
+-- (+) a b otherwise equals sum [a,b].
         
 main :: IO ();
 main = putStrLn $ "2 + 2 = " ++ show (2 + 2) ++ ".";
