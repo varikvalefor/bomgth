9011d28fdabc22a8f4906cb269aaadfdd1473854
Varik Valefor <varikvalefor@aol.com>
2021-08-23 20:34:07 -0400

    relativelypracticalstuff/exercises/graphplotter.hs is shortened.

--- relativelypracticalstuff/exercises/graphplotter.hs
+++ relativelypracticalstuff/exercises/graphplotter.hs
@@ -1,8 +1,7 @@
 main :: IO ();
-main = putStrLn ("The lecturee should modify this program such that " ++
-    "this program can use the terminal to plot a function at a " ++
-    "variable-defined width and height.") >>
-  putStrLn ("The function can be defined in the source code, " ++
-    "although brownie points are awarded if the end user can input" ++
-    "the function at runtime.") >>
-  putStrLn "However, the function MUST be editable.";
+main = putStrLn $ "The lecturee should modify this program such " ++
+  "that this program can use the terminal to plot a function at a " ++
+  "variable-defined width and height.\n" ++
+  "The function can be defined in the source code, although brownie " ++
+  "points are awarded if the end user can input the function at " ++
+  "runtime.\nHowever, the function MUST be editable.";
